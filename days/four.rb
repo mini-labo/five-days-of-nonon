807ff2a64cc0e6bc3d5bb639408f21d4ebb8e01b
jdgc <james@jdgc.dev>
2024-01-13 21:07:44 +0900

    four

---
+++ days/four.rb
@@ -0,0 +1,19 @@
+## 11:49AM UTC 11th Jan
+DAY_FOUR_CLOSING_TIME = '2024-01-13T11:49:00Z'
+
+## thank you friends 
+DAY_FOUR_ENTRANTS = [
+  '@yabishabi',
+  '@MrBoard420',
+  '@bexarbacoa',
+  '@itsGboii',
+  '@sssbbbbbbuuu',
+  '@sschwingg',
+  '@inabisou',
+  '@peqrio',
+  '@thebeefdog',
+  '@jebsl3y',
+  '@star11te',
+  '@influenzer_eth',
+  '@Davisteraz'
+]
